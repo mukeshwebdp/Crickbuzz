cdee9796d1c402dd642f024af5b95318737fafda
mukeshwebdp <mukesh.webdp@gmail.com>
2025-02-08 23:15:51 +0530

    add loop and state management

--- src/component/TopStories.jsx
+++ src/component/TopStories.jsx
@@ -1,55 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style/topStories.css'
+
+
 function TopStories() {
-  return (
+    const [data, setData] = useState({
+        img: 'cricket.jpg',
+        info: 'CT25: ICC meet adjourned after a brief discussion; to be reconvened soon'
+
+    })
+return (
     <>
         <div className="top-stories">
             <div className="stories-line">
                 <p>Top Stories</p>
             </div>
 
-            <div className="stories-info ">
-               <div className="card">
-                    <div className="img">
-                        <img src="cricket.jpg" alt="" />
+            <div className="stories-info">
+                {[...Array(4)].map((_, i) => (
+                    <div key={i} className="card">
+                        <div className="img">
+                            <img src={data.img} alt="" />
+                        </div>
+                        <div className="info">
+                            <p>{data.info}</p>
+                            <a>Read More</a>
+                        </div>
                     </div>
-                    <div className="info">
-                        <p>CT25: ICC meet adjourned after a brief discussion; to be reconvened soon</p>
-                        <p>Read More</p>
-                    </div>
-                </div>
-               <div className="card">
-                    <div className="img">
-                        <img src="cricket.jpg" alt="" />
-                    </div>
-                    <div className="info">
-                        <p>CT25: ICC meet adjourned after a brief discussion; to be reconvened soon</p>
-                        <p>Read More</p>
-                    </div>
-                </div>
-               <div className="card">
-                    <div className="img">
-                        <img src="cricket.jpg" alt="" />
-                    </div>
-                    <div className="info">
-                        <p>CT25: ICC meet adjourned after a brief discussion; to be reconvened soon</p>
-                        <p>Read More</p>
-                    </div>
-                </div>
-               <div className="card">
-                    <div className="img">
-                        <img src="cricket.jpg" alt="" />
-                    </div>
-                    <div className="info">
-                        <p>CT25: ICC meet adjourned after a brief discussion; to be reconvened soon</p>
-                        <p>Read More</p>
-                    </div>
-                </div>
+                ))}
             </div>
             <p className='left'>View All</p>
         </div>
     </>
-  )
+)
 }
 
 export default TopStories
